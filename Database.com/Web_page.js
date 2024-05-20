770f9b251b60b81acbd60e8431914b07d43e90b4
LalaCoder <ayushujjwal@yahoo.com>
2024-05-20 19:27:13 +0530

    Updating Databae.com

--- Database.com/Web_page.js
+++ Database.com/Web_page.js
@@ -1,1 +1,16 @@
-window.onload = () => {};
+window.onload = () => {
+
+    if ( window.location.pathname.includes( 'index.html' ) ) {
+
+        const buttons = document.body.querySelectorAll( 'button' );
+
+        var sign_in_button = buttons[ 0 ];
+        var sign_up_button = buttons[ 1 ];
+
+        sign_in_button.addEventListener( () => {});
+
+        sign_up_button.addEventListener( () => { return window.location.assign( './sign_up.html' ) });
+
+    } else if ( window.location.pathname.includes( 'sign_up.html' ) ) {}
+
+};
